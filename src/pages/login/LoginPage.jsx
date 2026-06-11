45fe1bd817127012564f553d82c0b4827bb56ecc
AndresLasala99 <al346108@fi365.ort.edu.uy>
2026-06-11 14:49:22 -0300

    Redirigir login con sesion activa

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,7 +1,15 @@
-﻿import LoginForm from "../../componentes/login/LoginForm.jsx"
+import { Navigate } from "react-router"
+import { useSelector } from "react-redux"
+import LoginForm from "../../componentes/login/LoginForm.jsx"
 import LayoutAutenticacion from "../../componentes/layout/LayoutAutenticacion.jsx"
 
 const LoginPage = () => {
+  const token = useSelector((state) => state.auth.token)
+
+  if (token) {
+    return <Navigate to="/dashboard" replace />
+  }
+
   return (
     <LayoutAutenticacion
       etiqueta="Leg Day Never"
